594b5c0a74ba996d1492e54eda3e89e19836f28c
abilashkrishnan1990 <abilashkrishnan1990@gmail.com>
2018-09-30 10:53:35 +0530

    third commit

--- classes/BankAccount.php
+++ classes/BankAccount.php
@@ -5,25 +5,24 @@ class BankAccount implements IfaceBankAccount
 
     private $balance = null;
 
-    public function __construct(Money $openingBalance)
-    {
+    public function __construct(Money $openingBalance){
         $this->balance = $openingBalance;
     }
 
-    public function balance()
-    {
+    public function balance(){
         return $this->balance;
     }
 
-    public function deposit(Money $amount)
-    {
-        //implement this method
+    public function deposit(Money $amount){
+        $amount  = $amount->value();
+        $balance = $this->balance->value();
+        $balance = $balance + $amount ;
+        $this->balance = new Money($balance);
     }
     public function withdraw(Money $amount){
         //implement this method
     }
-    public function transfer(Money $amount, BankAccount $account)
-    {
+    public function transfer(Money $amount, BankAccount $account){
         //implement this method
     }
 }
